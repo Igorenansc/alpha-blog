a828fe9f58594f54f5ed4f2db95dbf31e37a68dd
Igor Renan <igorenansc@gmail.com>
2022-04-13 20:31:09 -0300

    Add admin table on users

---
+++ db/migrate/20220413230713_add_admin_to_users.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Add admin table to existing users table, with a default value as false
+class AddAdminToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :admin, :boolean, default: false
+  end
+end
