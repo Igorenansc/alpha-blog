3ae1d8a0c9ff1b263b0d17d75b4a006aaf649517
Igor Renan <igorenansc@gmail.com>
2022-04-22 23:27:42 -0300

    Add association from UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_173126) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_195719) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
