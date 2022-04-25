ebc761cf4326eec920a8a830a45ee3bd3f61be6b
Igor Renan <igorenansc@gmail.com>
2022-04-24 23:31:32 -0300

    Add edit category view and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'Category was successfully updated.'
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
